970218cf9acf9d8359817aa93ceb8d57e35b3ec4
rerokim <baram876@naver.com>
2026-03-30 10:53:51 +0900

    3/30백업

---
+++ ch04/Assets/scripting/carcontroller.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class carcontroller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float speed = 0f;
+    Vector2 startpos;
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startpos = Input.mousePosition;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipelength = endPos.x - startpos.x;
+            speed = swipelength / 500f;
+        }
+        transform.Translate(speed, 0, 0);
+        speed *= 0.995f;
+    }
+}
