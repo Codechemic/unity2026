df5aa5e87830b7209487d68da606623fad0f99ee
rerokim <baram876@naver.com>
2026-04-13 09:54:00 +0900

    4/13 1교시 백업

--- ch05/Assets/20.script/arrowcontroller.cs
+++ ch05/Assets/20.script/arrowcontroller.cs
@@ -2,13 +2,30 @@ using UnityEngine;
 
 public class arrowcontroller : MonoBehaviour
 {
+    public GameObject player;
+
+    float minDistance = 1.1f;
+
     public float dropspeed = 0.1f;
+
+    private void Start()
+    {
+        player = GameObject.Find("player");
+    }
     // Update is called once per frame
     void Update()
     {
         transform.Translate(0, -dropspeed, 0);
 
         if (transform.position.y < -7)
+        {
+            Destroy(gameObject);
+        }
+
+        Vector2 p1 = transform.position;
+        Vector2 p2 = player.transform.position;
+        float distance = (p1 - p2).magnitude;
+        if (distance < minDistance)
         {
             Destroy(gameObject);
         }
